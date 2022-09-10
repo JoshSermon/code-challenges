a8aff400efad465a38b3065477f61de608017385
Josh Sermon <jsermon@LuckieATL4145.attlocal.net>
2022-09-09 23:10:16 -0400

    leet code 20 - valid parentheses

---
+++ Easy/valid-parentheses.js
@@ -0,0 +1,48 @@
+// LEETCODE QUESTION 20 - VALID PARENTHESES
+
+// Solution 1
+var isValid = function(s) {
+    const stack = [];
+    const parens = '() {} []';
+    let i = 0;
+
+    while(i < s.length) {
+        stack.push(s[i]);
+        i++;
+
+        let open = stack[stack.length - 2];
+        let closed = stack[stack.length - 1];
+
+        let potParens = open + closed;
+
+        if(parens.includes(potParens)) {
+            stack.pop();
+            stack.pop();
+        }
+    }
+    return stack.length === 0;
+}
+
+
+
+// Solution 2
+var isValid = function(s) {
+    let map = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    }
+    let arr = [];
+    for(let i = 0; i < s.length; i ++){
+        if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
+            arr.push(s[i]);
+        }
+        else{
+            if(arr[arr.length - 1] === map[s[i]]){
+                arr.pop();
+            }
+            else return false;
+        }
+    }
+    return arr.length === 0;
+};
