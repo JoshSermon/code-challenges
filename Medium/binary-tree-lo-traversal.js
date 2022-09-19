2c1d9908cdf2aa1e85204028d85a3e3cb231dad1
Josh Sermon <jsermon@LuckieATL4145.attlocal.net>
2022-09-18 20:59:45 -0400

    leet code 102 - binary tree level order traversal - 2nd solution

--- Medium/binary-tree-lo-traversal.js
+++ Medium/binary-tree-lo-traversal.js
@@ -11,19 +11,19 @@
 // result = [[3], [9, 20], [15, 7]]
 
 
-
+// Solution 1
 
 var levelOrder = function(root) {
-    if(!root) return [];        // Edge case
+    if(!root) return [];        // Edge case - If root is null return an empty array
 
-    const queue = [root];       // Set initializer or Start point
-    const result = [];          // result will be in an array
+    const queue = [root];       // initialize the queue with root
+    const result = [];          // declaring output array
 
     while(queue.length) {
-        let len = queue.length;
-        result.push(queue.map(node => node.val));
+        let amount = queue.length; // Get the length
+        result.push(queue.map(node => node.val)); 
 
-        while(len--) {
+        while(amount--) {
             let node = queue.shift();
             if(node.left) queue.push(node.left);
             if(node.right) queue.push(node.right);
@@ -35,5 +35,33 @@ var levelOrder = function(root) {
 };
 
 
+// Solution 2
 
-// Result will be an empty array []
+var levelOrder = function(root) {
+    if(!root) return []       // Edge Case - If root is null return an empty array
+    
+    const queue = [root] // initialize the queue with root
+    const levels = [] // declaring output array
+    
+    while(queue.length !== 0){
+       const queueLength = queue.length // Get the length prior to dequeueing
+       const currLevel = [] // Declare this level
+       // loop through to exhaust all options and only to include nodes at currLevel
+       for(let i = 0; i < queueLength; i++){
+           // Get next node
+           const current = queue.shift()
+           
+           if(current.left){
+               queue.push(current.left)
+           }
+           if(current.right){
+               queue.push(current.right)
+           }
+           // After we add left and right for current, we add to currLevel
+           currLevel.push(current.val)
+       }
+       // Level has been finished. Push into output array
+       levels.push(currLevel)
+   }
+    return levels
+}
