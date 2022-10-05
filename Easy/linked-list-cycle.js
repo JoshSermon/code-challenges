fa91861b286973222c30861b0e2f70de730dda2f
Josh Sermon <jsermon@LuckieATL4145.attlocal.net>
2022-10-05 13:56:49 -0400

    leet code 141 - linked list cycle I

--- Easy/linked-list-cycle.js
+++ Easy/linked-list-cycle.js
@@ -15,3 +15,22 @@ var hasCycle = function(head) {
     
     return false;
 };
+
+
+// Solution 2
+
+var hasCycle = function(head) {
+    if(!head) return false;
+    let slow = head;
+    let fast = head;
+     
+     while(fast !== null && fast.next !== null) {
+         fast = fast.next.next;
+         slow = slow.next;
+         
+         if (slow === fast) {
+             return true;
+         }
+     }
+             return false;
+ };
